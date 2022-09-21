53050c63e0e0075c817e6254caacdaaacc1ab17c
UndissyaPutri <undissyaputri@gmail.com>
2022-09-21 16:23:01 +0700

    update tugas 3

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_xml
+from mywatchlist.views import show_mywatchlist, show_mywatchlist_json, show_mywatchlist_xml, show_json_by_id, show_xml_by_id
 
 urlpatterns = [
     path('', show_mywatchlist, name='show_mywatchlist'),
     path('html/', show_mywatchlist, name='show_mywatchlist'),
     path('json/', show_mywatchlist_json, name='show_mywatchlist_json'),
     path('xml/', show_mywatchlist_xml, name='show_mywatchlist_xml'),
+    path('json/<int:id>', show_json_by_id, name='show_mywatchlist_json'),
+    path('xml/<int:id>', show_xml_by_id, name='show_mywatchlist_xml'),
 ]
